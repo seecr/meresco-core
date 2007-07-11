2e66f5a24d55a45902e42526a7e7dec5b2fcd911
vanschelven <vanschelven@83e7a582-4515-4195-8f39-222cbda6297e>
2007-07-11 09:24:55 +0000

    kvS/EG: Copied observable and test from cq2utils, rev. 5335
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@106 83e7a582-4515-4195-8f39-222cbda6297e

---
+++ src/framework/observable.py
@@ -0,0 +1,106 @@
+## begin license ##
+#
+#    "CQ2 Utils" (cq2utils) is a package with a wide range of valuable tools.
+#    Copyright (C) 2005, 2006 Seek You Too B.V. (CQ2) http://www.cq2.nl
+#
+#    This file is part of "CQ2 Utils".
+#
+#    "CQ2 Utils" is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    "CQ2 Utils" is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with "CQ2 Utils"; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
+from traceback import format_exception
+from sys import exc_info
+
+class Defer:
+	def __init__(self, observable, defereeType):
+		self._observable = observable
+		self._defereeType = defereeType
+	
+	def __getattr__(self, attr):
+		return self._defereeType(self._observable._observers, attr)
+
+class DeferredFunction:
+	def __init__(self, delegates, attr):
+		self._delegates = delegates
+		self._attr = attr
+		
+class AllFunction(DeferredFunction):
+	def __call__(self, *args, **kwargs):
+		result = None
+		for delegate in self._delegates:
+			if hasattr(delegate, self._attr):
+				result = getattr(delegate, self._attr)(*args, **kwargs) or result
+		return result
+
+class AnyFunction(DeferredFunction):
+	def __call__(self, *args, **kwargs):
+		result = None
+		for delegate in self._delegates:
+			if hasattr(delegate, self._attr):
+				result = getattr(delegate, self._attr)(*args, **kwargs)
+				if result:
+					return result
+		return result
+
+class Observable:
+	def __init__(self):
+		self._observers = []
+		self.all = Defer(self, AllFunction)
+		self.any = Defer(self, AnyFunction)
+
+	def addObserver(self, observer):
+		self._observers.append(observer)
+
+	def _notifyObservers(self, __returnResult__, *args, **kwargs):
+		i = 0
+		while i < len(self._observers):
+			result = self._observers[i].notify(*args, **kwargs)
+			if __returnResult__ and result != None:
+				return result
+			i += 1
+				
+	def changed(self, *args, **kwargs):
+		return self._notifyObservers(False, *args, **kwargs)
+				
+	def process(self, *args, **kwargs):
+		return self._notifyObservers(True, *args, **kwargs)
+
+class Function:
+	
+	def __init__(self, observable):
+		self._observable = observable
+		self._observable.addObserver(self)
+		
+	def notify(self, *args, **kwargs):
+		self._result = args #kwargs is nog een vraagteken voor mij (KVS)
+		if len(self._result) == 0:
+			self._result = None
+		elif len(self._result) == 1:
+			self._result = self._result[0]
+		
+	def __call__(self, *args, **kwargs):
+		self._observable.notify(*args, **kwargs) # hier is **kwargs triviaal
+		return self._result
+		
+class FunctionObservable(Observable):
+	#ik weet het, YAGNI, maar het is zooooo mooi symmetrisch - wilde toch even laten zien dat dit het soort dingen is wat we kunnen doen en denk dat we het nut vrij snel tegenkomen. Zo niet dan mag hij in het vuilnisvat
+	
+	def __init__(self, function):
+		Observable.__init__(self)
+		self._function = function
+	
+	def notify(self, *args, **kwargs):
+		results = self._function(*args, **kwargs)
+		self.changed(*results)
---
+++ test/observabletest.py
@@ -0,0 +1,211 @@
+## begin license ##
+#
+#    "CQ2 Utils" (cq2utils) is a package with a wide range of valuable tools.
+#    Copyright (C) 2005, 2006 Seek You Too B.V. (CQ2) http://www.cq2.nl
+#
+#    This file is part of "CQ2 Utils".
+#
+#    "CQ2 Utils" is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    "CQ2 Utils" is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with "CQ2 Utils"; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
+
+from observable import Observable, Function, FunctionObservable
+from calltrace import CallTrace
+import unittest
+
+class ObservableTest(unittest.TestCase):
+	
+	def testNotifications(self):
+		observable = Observable()
+		one = MockObserver()
+		two = MockObserver()
+		observable.addObserver(one)
+		observable.addObserver(two)
+		observable.changed("A")
+		observable.changed("B", "C")
+		self.assertEquals([("A",), ("B", "C")], one.notifications)
+		self.assertEquals([("A",), ("B", "C")], two.notifications)
+		
+	def testProcess(self):
+		observable = Observable()
+		observerOne = CallTrace('ObserverOne')
+		observerOne.returnValues['notify'] = True
+		observerTwo = CallTrace('ObserverTwo')
+		observerTwo.returnValues['notify'] = 1
+		observerThree = CallTrace('ObserverThree')
+		observerThree.returnValues['notify'] = 'yes'
+		
+		observable.addObserver(observerOne)
+		observable.addObserver(observerTwo)
+		observable.addObserver(observerThree)
+		
+		result = observable.process('This is a cool test')
+		
+		self.assertEquals(True, result)
+		self.assertEquals(1, len(observerOne.calledMethods))
+		self.assertEquals(0, len(observerTwo.calledMethods))
+		self.assertEquals(0, len(observerThree.calledMethods))
+		
+	def testProcessNoneReturnValues(self):
+		observable = Observable()
+		observerOne = CallTrace('Observer')
+		observerOne.returnValues['notify'] = None
+		observerTwo = CallTrace('ObserverTwo')
+		observerTwo.returnValues['notify'] = ''
+		
+		observable.addObserver(observerOne)
+		observable.addObserver(observerTwo)
+		
+		result = observable.process('This is a cool test')
+		
+		self.assertEquals('', result)
+		
+	def testFunctionWrapperOneResultValue(self):
+		class MockExtendedObservable(Observable):
+			def notify(self, arg0, arg1):
+				self.changed("The Result " + arg0)
+		f = Function(MockExtendedObservable())
+		self.assertEquals("The Result A", f("A", "B"))
+		
+	def testFunctionWrapperNoResultValue(self):
+		class MockExtendedObservable(Observable):
+			def notify(self, arg0, arg1):
+				self.changed()
+		f = Function(MockExtendedObservable())
+		self.assertEquals(None, f("A", "B"))
+		
+	def testFunctionWrapperNResultValues(self):
+		class MockExtendedObservable(Observable):
+			def notify(self, arg0, arg1):
+				self.changed("Extended " + arg0, "Extended " + arg1, "Additional Argument")
+		f = Function(MockExtendedObservable())
+		self.assertEquals(("Extended A", "Extended B", "Additional Argument"), f("A", "B"))
+		
+	def testFunctionObservable(self):
+		function = lambda x: x
+		
+		observable = FunctionObservable(function)
+		observer = MockObserver()
+		observable.addObserver(observer)
+		
+		observable.notify("A")
+		self.assertEquals([("A",)], observer.notifications)
+	
+	def testFunctionObservableTupleResult(self):
+		function = lambda arg0: ("Extended " + arg0, "Additional Argument")
+		
+		observable = FunctionObservable(function)
+		one = MockObserver()
+		observable.addObserver(one)
+		
+		observable.notify("A")
+		self.assertEquals([("Extended A", "Additional Argument")], one.notifications)
+		
+	def testAll(self):
+		observable = Observable()
+		
+		observerAB = ObserverAB()
+		observerA = ObserverA()
+		doesNotReturn = DoesNotReturn()
+
+		observable.addObserver(observerAB)
+		observable.addObserver(observerA)
+		observable.addObserver(doesNotReturn)
+		
+		resultA = observable.all.methodA(0)
+		resultB = observable.all.methodB(1, 2)
+		self.assertEquals([
+			("Method A", (0,)),
+			("Method B", (1, 2))], observerAB.notifications)
+		self.assertEquals([("Method A", (0,))], observerA.notifications)
+		self.assertEquals([("Method A", (0,))], doesNotReturn.notifications)
+		self.assertEquals("A.methodA", resultA)
+		self.assertEquals("AB.methodB", resultB)
+			
+	def testAny(self):
+		observable = Observable()
+		
+		doesNotReturn = DoesNotReturn()
+		observerA = ObserverA()
+		observerAB = ObserverAB()
+		
+		observable.addObserver(doesNotReturn)
+		observable.addObserver(observerA)
+		observable.addObserver(observerAB)
+		
+		resultA = observable.any.methodA(0)
+		resultB = observable.any.methodB(1, 2)
+		self.assertEquals([("Method A", (0,))], doesNotReturn.notifications)
+		self.assertEquals([("Method A", (0,))], observerA.notifications)
+		self.assertEquals([("Method B", (1, 2))], observerAB.notifications)
+		
+		self.assertEquals("A.methodA", resultA)
+		self.assertEquals("AB.methodB", resultB)
+		
+		#add a test for None-result
+	
+	def testAllException(self):
+		observable = Observable()
+		class ExceptionRaiser(MockObserver):
+			def mayRaiseException(self, *args):
+				raise TestException
+		class Safe(MockObserver):
+			def mayRaiseException(self, *args):
+				self.notifications.append(("mayRaiseException", args))
+		observable.addObserver(ExceptionRaiser())
+		safe = Safe()
+		observable.addObserver(safe)
+		try:
+			observable.all.mayRaiseException()
+			self.fail()
+		except TestException:
+			pass
+		self.assertEquals([], safe.notifications)
+		
+class TestException(Exception):
+	pass
+
+class MockObserver:
+	
+	def __init__(self):
+		self.notifications = []
+	
+	def notify(self, *args):
+		self.notifications.append(args)
+		
+	def notifyRaisesException(self, *args):
+		self.notifications.append(args)
+		raise TestException("notifyRaisesException")
+	
+class ObserverA(MockObserver):
+	
+	def methodA(self, *args):
+		self.notifications.append(("Method A", args))
+		return "A.methodA"
+	
+class ObserverAB(MockObserver):
+	
+	def methodA(self, *args):
+		self.notifications.append(("Method A", args))
+		return "AB.methodA"
+	
+	def methodB(self, *args):
+		self.notifications.append(("Method B", args))
+		return "AB.methodB"
+	
+class DoesNotReturn(MockObserver):
+	
+	def methodA(self, *args):
+		self.notifications.append(("Method A", args))
