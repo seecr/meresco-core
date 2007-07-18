3e454ef6f4c8026bdd850a9f89dfbc99ba52bdce
thijsjanssen <thijsjanssen@83e7a582-4515-4195-8f39-222cbda6297e>
2007-07-18 09:01:06 +0000

    TJ: changed develop.d --> deps.d
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@145 83e7a582-4515-4195-8f39-222cbda6297e

--- test/alltests.py
+++ test/alltests.py
@@ -30,7 +30,7 @@ import os, sys
 os.system('find .. -name "*.pyc" | xargs rm -f')
 
 from glob import glob
-for path in glob('../develop.d/*'):
+for path in glob('../deps.d/*'):
 	sys.path.insert(0, path)
 sys.path.insert(0, "..")
 
