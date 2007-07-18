8babd6019f86ed01124e335af8e5abd71a1b2738
thijsjanssen <thijsjanssen@83e7a582-4515-4195-8f39-222cbda6297e>
2007-07-18 09:44:18 +0000

    TJ: applied license
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@149 83e7a582-4515-4195-8f39-222cbda6297e

--- src/components/drilldown/cpp/alltests.py
+++ src/components/drilldown/cpp/alltests.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python
+## begin license ##
+#
+#    Meresco Core is part of Meresco.
+#    Copyright (C) SURF Foundation. http://www.surf.nl
+#    Copyright (C) Seek You Too B.V. (CQ2) http://www.cq2.nl
+#    Copyright (C) SURFnet. http://www.surfnet.nl
+#    Copyright (C) Stichting Kennisnet Ict op school. 
+#       http://www.kennisnetictopschool.nl
+#
+#    This file is part of Meresco Core.
+#
+#    Meresco Core is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    Meresco Core is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Meresco Core; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
 #
 # Generated with:
 #
--- src/framework/typedobservable.py
+++ src/framework/typedobservable.py
@@ -1,22 +1,26 @@
 ## begin license ##
 #
-#    "CQ2 Utils" (cq2utils) is a package with a wide range of valuable tools.
-#    Copyright (C) 2005, 2006 Seek You Too B.V. (CQ2) http://www.cq2.nl
+#    Meresco Core is part of Meresco.
+#    Copyright (C) SURF Foundation. http://www.surf.nl
+#    Copyright (C) Seek You Too B.V. (CQ2) http://www.cq2.nl
+#    Copyright (C) SURFnet. http://www.surfnet.nl
+#    Copyright (C) Stichting Kennisnet Ict op school. 
+#       http://www.kennisnetictopschool.nl
 #
-#    This file is part of "CQ2 Utils".
+#    This file is part of Meresco Core.
 #
-#    "CQ2 Utils" is free software; you can redistribute it and/or modify
+#    Meresco Core is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation; either version 2 of the License, or
 #    (at your option) any later version.
 #
-#    "CQ2 Utils" is distributed in the hope that it will be useful,
+#    Meresco Core is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with "CQ2 Utils"; if not, write to the Free Software
+#    along with Meresco Core; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
