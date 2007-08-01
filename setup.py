c74fbd572fc4d96cd7240dcd96a43b8d6de5bde1
thijsjanssen <thijsjanssen@83e7a582-4515-4195-8f39-222cbda6297e>
2007-08-01 14:41:52 +0000

    EG/KvS: added extension and pkg data
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@209 83e7a582-4515-4195-8f39-222cbda6297e

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 from distutils.core import setup
+from distutils.extension import Extension
 
 setup(
     name='meresco',
     packages=[
-'meresco',
-'meresco.legacy',
-'meresco.legacy.plugins',
-'meresco.components',
-'meresco.components.http',
-'meresco.components.http.oai',
-'meresco.components.http.oai.data',
-'meresco.components.http.sru',
-'meresco.components.drilldown',
-'meresco.components.drilldown.cpp',
-'meresco.components.lucene',
-'meresco.framework'
-])
+        'meresco',
+        'meresco.legacy',
+        'meresco.legacy.plugins',
+        'meresco.components',
+        'meresco.components.http',
+        'meresco.components.http.oai',
+        'meresco.components.http.sru',
+        'meresco.components.drilldown',
+        'meresco.components.drilldown.cpp',
+        'meresco.components.lucene',
+        'meresco.framework'
+    ],
+    package_data={
+        'meresco.components.http.oai': ['data/*']
+    },
+    ext_modules=[
+        Extension("meresco.components.drilldown.cpp.bitarray", ["meresco/components/drilldown/cpp/BitArray.cpp"])]
+)
