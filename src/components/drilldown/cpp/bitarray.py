bbf9cf07d128be01878e6f6f6ed17d884e362100
thijsjanssen <thijsjanssen@83e7a582-4515-4195-8f39-222cbda6297e>
2007-08-02 14:29:11 +0000

    KvS/EG/JJ: Added bitarray.py to make bitarray (partially) independent of swig
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@217 83e7a582-4515-4195-8f39-222cbda6297e

---
+++ src/components/drilldown/cpp/bitarray.py
@@ -0,0 +1,83 @@
+# This file was created automatically by SWIG 1.3.29.
+# Don't modify this file, modify the SWIG interface instead.
+# This file is compatible with both classic and new-style classes.
+
+import _bitarray
+import new
+new_instancemethod = new.instancemethod
+def _swig_setattr_nondynamic(self,class_type,name,value,static=1):
+    if (name == "thisown"): return self.this.own(value)
+    if (name == "this"):
+        if type(value).__name__ == 'PySwigObject':
+            self.__dict__[name] = value
+            return
+    method = class_type.__swig_setmethods__.get(name,None)
+    if method: return method(self,value)
+    if (not static) or hasattr(self,name):
+        self.__dict__[name] = value
+    else:
+        raise AttributeError("You cannot add attributes to %s" % self)
+
+def _swig_setattr(self,class_type,name,value):
+    return _swig_setattr_nondynamic(self,class_type,name,value,0)
+
+def _swig_getattr(self,class_type,name):
+    if (name == "thisown"): return self.this.own()
+    method = class_type.__swig_getmethods__.get(name,None)
+    if method: return method(self)
+    raise AttributeError,name
+
+def _swig_repr(self):
+    try: strthis = "proxy of " + self.this.__repr__()
+    except: strthis = ""
+    return "<%s.%s; %s >" % (self.__class__.__module__, self.__class__.__name__, strthis,)
+
+import types
+try:
+    _object = types.ObjectType
+    _newclass = 1
+except AttributeError:
+    class _object : pass
+    _newclass = 0
+del types
+
+
+class DenseBitArray(_object):
+    __swig_setmethods__ = {}
+    __setattr__ = lambda self, name, value: _swig_setattr(self, DenseBitArray, name, value)
+    __swig_getmethods__ = {}
+    __getattr__ = lambda self, name: _swig_getattr(self, DenseBitArray, name)
+    __repr__ = _swig_repr
+    def __init__(self, *args): 
+        this = _bitarray.new_DenseBitArray(*args)
+        try: self.this.append(this)
+        except: self.this = this
+    def cardinality(*args): return _bitarray.DenseBitArray_cardinality(*args)
+    def set(*args): return _bitarray.DenseBitArray_set(*args)
+    def get(*args): return _bitarray.DenseBitArray_get(*args)
+    def combinedCardinality(*args): return _bitarray.DenseBitArray_combinedCardinality(*args)
+    __swig_destroy__ = _bitarray.delete_DenseBitArray
+    __del__ = lambda self : None;
+DenseBitArray_swigregister = _bitarray.DenseBitArray_swigregister
+DenseBitArray_swigregister(DenseBitArray)
+
+class SparseBitArray(_object):
+    __swig_setmethods__ = {}
+    __setattr__ = lambda self, name, value: _swig_setattr(self, SparseBitArray, name, value)
+    __swig_getmethods__ = {}
+    __getattr__ = lambda self, name: _swig_getattr(self, SparseBitArray, name)
+    __repr__ = _swig_repr
+    def __init__(self, *args): 
+        this = _bitarray.new_SparseBitArray(*args)
+        try: self.this.append(this)
+        except: self.this = this
+    def cardinality(*args): return _bitarray.SparseBitArray_cardinality(*args)
+    def set(*args): return _bitarray.SparseBitArray_set(*args)
+    def combinedCardinality(*args): return _bitarray.SparseBitArray_combinedCardinality(*args)
+    __swig_destroy__ = _bitarray.delete_SparseBitArray
+    __del__ = lambda self : None;
+SparseBitArray_swigregister = _bitarray.SparseBitArray_swigregister
+SparseBitArray_swigregister(SparseBitArray)
+
+
+
