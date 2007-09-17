fa855946a0c8db8a67d1ca12d0bc71032d36f278
thijsjanssen <thijsjanssen@83e7a582-4515-4195-8f39-222cbda6297e>
2007-09-17 14:03:36 +0000

    EG/KvS: fixed deps.txt; meanwhile, new swik generated sources
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@318 83e7a582-4515-4195-8f39-222cbda6297e

--- meresco/components/drilldown/cpp/bitarray.py
+++ meresco/components/drilldown/cpp/bitarray.py
@@ -1,30 +1,4 @@
-## begin license ##
-#
-#    Meresco Core is part of Meresco.
-#    Copyright (C) 2007 SURF Foundation. http://www.surf.nl
-#    Copyright (C) 2007 Seek You Too B.V. (CQ2) http://www.cq2.nl
-#    Copyright (C) 2007 SURFnet. http://www.surfnet.nl
-#    Copyright (C) 2007 Stichting Kennisnet Ict op school. 
-#       http://www.kennisnetictopschool.nl
-#
-#    This file is part of Meresco Core.
-#
-#    Meresco Core is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
-#
-#    Meresco Core is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with Meresco Core; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#
-## end license ##
-# This file was created automatically by SWIG 1.3.28.
+# This file was created automatically by SWIG 1.3.29.
 # Don't modify this file, modify the SWIG interface instead.
 # This file is compatible with both classic and new-style classes.
 
@@ -53,6 +27,11 @@ def _swig_getattr(self,class_type,name):
     if method: return method(self)
     raise AttributeError,name
 
+def _swig_repr(self):
+    try: strthis = "proxy of " + self.this.__repr__()
+    except: strthis = ""
+    return "<%s.%s; %s >" % (self.__class__.__module__, self.__class__.__name__, strthis,)
+
 import types
 try:
     _object = types.ObjectType
@@ -68,11 +47,8 @@ class DenseBitArray(_object):
     __setattr__ = lambda self, name, value: _swig_setattr(self, DenseBitArray, name, value)
     __swig_getmethods__ = {}
     __getattr__ = lambda self, name: _swig_getattr(self, DenseBitArray, name)
-    def __repr__(self):
-        try: strthis = "at 0x%x" %( self.this, ) 
-        except: strthis = "" 
-        return "<%s.%s; proxy of C++ DenseBitArray instance %s>" % (self.__class__.__module__, self.__class__.__name__, strthis,)
-    def __init__(self, *args):
+    __repr__ = _swig_repr
+    def __init__(self, *args): 
         this = _bitarray.new_DenseBitArray(*args)
         try: self.this.append(this)
         except: self.this = this
@@ -82,18 +58,16 @@ class DenseBitArray(_object):
     def combinedCardinality(*args): return _bitarray.DenseBitArray_combinedCardinality(*args)
     __swig_destroy__ = _bitarray.delete_DenseBitArray
     __del__ = lambda self : None;
-_bitarray.DenseBitArray_swigregister(DenseBitArray)
+DenseBitArray_swigregister = _bitarray.DenseBitArray_swigregister
+DenseBitArray_swigregister(DenseBitArray)
 
 class SparseBitArray(_object):
     __swig_setmethods__ = {}
     __setattr__ = lambda self, name, value: _swig_setattr(self, SparseBitArray, name, value)
     __swig_getmethods__ = {}
     __getattr__ = lambda self, name: _swig_getattr(self, SparseBitArray, name)
-    def __repr__(self):
-        try: strthis = "at 0x%x" %( self.this, ) 
-        except: strthis = "" 
-        return "<%s.%s; proxy of C++ SparseBitArray instance %s>" % (self.__class__.__module__, self.__class__.__name__, strthis,)
-    def __init__(self, *args):
+    __repr__ = _swig_repr
+    def __init__(self, *args): 
         this = _bitarray.new_SparseBitArray(*args)
         try: self.this.append(this)
         except: self.this = this
@@ -102,7 +76,8 @@ class SparseBitArray(_object):
     def combinedCardinality(*args): return _bitarray.SparseBitArray_combinedCardinality(*args)
     __swig_destroy__ = _bitarray.delete_SparseBitArray
     __del__ = lambda self : None;
-_bitarray.SparseBitArray_swigregister(SparseBitArray)
+SparseBitArray_swigregister = _bitarray.SparseBitArray_swigregister
+SparseBitArray_swigregister(SparseBitArray)
 
 
 
