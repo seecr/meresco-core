15be43b8d959cbd588bbcc58376ea01dd319f422
thijsjanssen <thijsjanssen@83e7a582-4515-4195-8f39-222cbda6297e>
2007-10-23 15:25:39 +0000

    TJ/JJ: added some import statements.
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@377 83e7a582-4515-4195-8f39-222cbda6297e

--- meresco/components/dictionary/__init__.py
+++ meresco/components/dictionary/__init__.py
@@ -1,3 +1,6 @@
 from documentdict import DocumentDict, DocumentField
 from transform import Transform
 from xml2dict import Xml2Dict
+from dict2doc import Dict2Doc
+from dictaggregator import DictAggregator
+from dictidentity import DictIdentity
