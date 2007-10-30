67bb40e2deb6bdd0d036fbf3161cdfb988c547f2
johan <johan@83e7a582-4515-4195-8f39-222cbda6297e>
2007-10-30 14:17:04 +0000

    KvS/JJ: Factored out rawdocsets per field
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@389 83e7a582-4515-4195-8f39-222cbda6297e

--- meresco/components/drilldown/lucenerawdocsets.py
+++ meresco/components/drilldown/lucenerawdocsets.py
@@ -33,38 +33,38 @@ class LuceneRawDocSets(object):
 
     def getDocSets(self):
         for fieldName in self._fieldNames:
-            yield (fieldName, self._docSetsForFieldLucene(fieldName))
+            yield (fieldName, luceneRawDocSetsForField(self._reader, fieldName))
 
     def docCount(self):
         return self._reader.numDocs()
 
-    def _docSetsForFieldLucene(self, fieldName):
-        termDocs = self._reader.termDocs()
-        termEnum = self._reader.terms(PyLucene.Term(fieldName, ''))
-        #IndexReader.terms returns something of the following form, if fieldname == fieldname3
-        #fieldname3 'abla'
-        #fieldname3 'bb'
-        #fielname3 'zz'
-        #fieldname4 'aa'
+def luceneRawDocSetsForField(reader, fieldName):
+    termDocs = reader.termDocs()
+    termEnum = reader.terms(PyLucene.Term(fieldName, ''))
+    #IndexReader.terms returns something of the following form, if fieldname == fieldname3
+    #fieldname3 'abla'
+    #fieldname3 'bb'
+    #fielname3 'zz'
+    #fieldname4 'aa'
 
-        #The enum has the following (weird) behaviour: the internal pointer references
-        #the first element by default, but when there are no elements it references a
-        #None element. Therefor we have to check "if not term".
-        #We use a "do ... while" idiom because calling next would advance the internal
-        #pointer, resulting in a missed first element
+    #The enum has the following (weird) behaviour: the internal pointer references
+    #the first element by default, but when there are no elements it references a
+    #None element. Therefor we have to check "if not term".
+    #We use a "do ... while" idiom because calling next would advance the internal
+    #pointer, resulting in a missed first element
 
-        while True:
-            term = termEnum.term()
-            if not term or term.field() != fieldName:
-                break
-            termDocs.seek(term)
+    while True:
+        term = termEnum.term()
+        if not term or term.field() != fieldName:
+            break
+        termDocs.seek(term)
 
-            yield (term.text(), self._generateDocIds(termDocs))
-            if not termEnum.next():
-                break
+        yield (term.text(), _generateDocIds(termDocs))
+        if not termEnum.next():
+            break
 
-    def _generateDocIds(self, termDocs):
-        result = []
-        while termDocs.next():
-            result.append(termDocs.doc())
-        return result
+def _generateDocIds(termDocs):
+    result = []
+    while termDocs.next():
+        result.append(termDocs.doc())
+    return result
