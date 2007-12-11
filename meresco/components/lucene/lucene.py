181b1d6a953e5fc1780fa663797fe047b2ba9497
thijsjanssen <thijsjanssen@83e7a582-4515-4195-8f39-222cbda6297e>
2007-12-11 10:01:28 +0000

    KvS/JJ: Lucene major cleanup Used cqlParseTreeToLuceneQuery (skipping extra parse step)
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@436 83e7a582-4515-4195-8f39-222cbda6297e

--- meresco/components/lucene/lucene.py
+++ meresco/components/lucene/lucene.py
@@ -4,7 +4,7 @@
 #    Copyright (C) 2007 SURF Foundation. http://www.surf.nl
 #    Copyright (C) 2007 Seek You Too B.V. (CQ2) http://www.cq2.nl
 #    Copyright (C) 2007 SURFnet. http://www.surfnet.nl
-#    Copyright (C) 2007 Stichting Kennisnet Ict op school. 
+#    Copyright (C) 2007 Stichting Kennisnet Ict op school.
 #       http://www.kennisnetictopschool.nl
 #
 #    This file is part of Meresco Core.
@@ -30,62 +30,13 @@ from warnings import warn
 import PyLucene
 import xml.sax.saxutils
 
-from cqlparser.lucenecomposer import compose as cqlAbstractSyntaxTreeToLucene
+from meresco.components.lucene.cqlparsetreetolucenequery import compose as composeLuceneQuery
 
 from meresco.components.lucene.hits import Hits
 from meresco.components.lucene.document import IDFIELD, CONTENTFIELD
 
-
-DEFAULT_OFFSET = 0
-DEFAULT_COUNT = 10
-
 class LuceneException(Exception):
     pass
-
-class LuceneQuery:
-    def __init__(self, aLuceneIndex, aQueryString, anOffset = DEFAULT_OFFSET,
-                aCount = DEFAULT_COUNT, sortBy = None, sortDescending = None):
-        """Very very deprecated"""
-        self._index = aLuceneIndex
-        self._offset = anOffset
-        self._count = aCount
-        self._queryString = aQueryString
-        self._hitCount = 0
-        self._batchSize = 0
-        self._sortBy = sortBy
-        self._sortDescending = sortDescending
-        analyzer = PyLucene.StandardAnalyzer()
-        queryParser = PyLucene.QueryParser(CONTENTFIELD, analyzer)
-        queryParser.setDefaultOperator(PyLucene.QueryParser.Operator.AND)
-        self._query = queryParser.parse(self._queryString)
-
-    def perform(self):
-        hits = self._index.search(self._getQuery(), self._getSort())
-        self._hitCount = hits.length()
-        batch = range(self._offset, min(self._offset + self._count, self._hitCount))
-        for h in batch:
-            yield hits.doc(h).get(IDFIELD)
-        self._batchSize = len(batch)
-
-    def getBatchSize(self):
-        return self._batchSize
-
-    def getOffset(self):
-        return self._offset
-
-    def getCount(self):
-        return self._count
-
-    def getHitCount(self):
-        return self._hitCount
-
-    def _getSort(self):
-        sortDir = bool(self._sortDescending)
-        return self._sortBy and \
-            PyLucene.Sort(self._sortBy, sortDir) or None
-
-    def _getQuery(self):
-        return self._query
 
 class LuceneIndex:
 
@@ -100,32 +51,14 @@ class LuceneIndex:
             self._getWriter(createIndex = True)
         self._indexChanged = False
 
-    def query(self, aQuery):
-        return self._getSearcher().search(aQuery)
-
-    def queryWith(self, aLuceneQuery):
-        for hit in aLuceneQuery.search():
-            yield hit
-
     def deleteID(self, anId):
         self._getReader().deleteDocuments(PyLucene.Term(IDFIELD, anId))
-
-    def countField(self, fieldName):
-        """Deprecated (only used by legacay.plugins.fieldcountplugin) when killing kill TermIter too"""
-        countDict = {}
-        termEnum = self._getReader().terms(PyLucene.Term(fieldName, ''))
-        termIter = TermIter(termEnum, fieldName)
-        for term in termIter:
-            countDict[term.text()] = self._getReader().docFreq(term)
-        return countDict.items()
 
     def executeQuery(self, pyLuceneQuery, sortBy=None, sortDescending=None):
         return Hits(self._getSearcher(), pyLuceneQuery, self._getPyLuceneSort(sortBy, sortDescending))
 
     def executeCQL(self, cqlAbstractSyntaxTree, sortBy=None, sortDescending=None):
-        return Hits(self._getSearcher(),
-            self._parseLuceneQueryString(cqlAbstractSyntaxTreeToLucene(cqlAbstractSyntaxTree)),
-            self._getPyLuceneSort(sortBy, sortDescending))
+        return executeQuery(composeLuceneQuery(cqlAbstractSyntaxTree), sortBy, sortDescending)
 
     def addToIndex(self, aDocument):
         aDocument.validate()
@@ -136,19 +69,12 @@ class LuceneIndex:
             self._getWriter().optimize()
         finally:
             self.close()
-        
 
     def docCount(self):
         return self._getReader().numDocs()
 
     def _getPyLuceneSort(self, sortBy, sortDescending):
         return sortBy and PyLucene.Sort(sortBy, bool(sortDescending)) or None
-
-    def _parseLuceneQueryString(self, luceneQueryString):
-        analyzer = PyLucene.StandardAnalyzer()
-        queryParser = PyLucene.QueryParser(CONTENTFIELD, analyzer)
-        queryParser.setDefaultOperator(PyLucene.QueryParser.Operator.AND)
-        return queryParser.parse(luceneQueryString)
 
     def close(self):
         if self.__reader:
@@ -182,20 +108,3 @@ class LuceneIndex:
     def __del__(self):
         self.close()
 
-class TermIter:
-    """Deprecated thing for countfield"""
-    def __init__(self, termEnum, fieldName):
-        self._termEnum = termEnum
-        self._nextTerm = termEnum.term()
-        self._fieldName = fieldName
-
-    def __iter__(self):
-        return self
-
-    def next(self):
-        if self._nextTerm == None or self._nextTerm.field() != self._fieldName:
-            raise StopIteration()
-        result, self._nextTerm = self._nextTerm, None
-        if self._termEnum.next():
-            self._nextTerm = self._termEnum.term()
-        return result
--- test/lucenetest.py
+++ test/lucenetest.py
@@ -5,7 +5,7 @@
 #    Copyright (C) 2007 SURF Foundation. http://www.surf.nl
 #    Copyright (C) 2007 Seek You Too B.V. (CQ2) http://www.cq2.nl
 #    Copyright (C) 2007 SURFnet. http://www.surfnet.nl
-#    Copyright (C) 2007 Stichting Kennisnet Ict op school. 
+#    Copyright (C) 2007 Stichting Kennisnet Ict op school.
 #       http://www.kennisnetictopschool.nl
 #
 #    This file is part of Meresco Core.
@@ -29,12 +29,13 @@
 # Lucene Test
 #
 
-import unittest
 from tempfile import mkdtemp, gettempdir
 import os
 from os.path import isfile, join
 from shutil import rmtree
 import PyLucene
+from cq2utils import CQ2TestCase
+
 from meresco.components.lucene.document import Document
 
 from meresco.components.lucene.lucene import LuceneIndex
@@ -42,13 +43,7 @@ from meresco.components.lucene.document import IDFIELD
 
 from PyLucene import Document as PyDocument, Field, IndexReader, IndexWriter, StandardAnalyzer, Term
 
-class LuceneTest(unittest.TestCase):
-
-    def _removeWhiteSpace(self, aString):
-        return ''.join(aString.split())
-
-    def assertEqualsWS(self, s1, s2):
-        self.assertEquals(self._removeWhiteSpace(s1), self._removeWhiteSpace(s2))
+class LuceneTest(CQ2TestCase):
 
     def setUp(self):
         self._tempdir = gettempdir()+'/testing'
@@ -64,6 +59,16 @@ class LuceneTest(unittest.TestCase):
         self.assertEquals(os.path.isdir(self.directoryName), True)
         self.assertTrue(IndexReader.indexExists(self.directoryName))
 
+    def testAddToIndex(self):
+        myDocument = Document('0123456789')
+        myDocument.addIndexedField('title', 'een titel')
+        self._luceneIndex.addToIndex(myDocument)
+
+        query = PyLucene.QueryParser('title', PyLucene.StandardAnalyzer()).parse('titel')
+        hits = self._luceneIndex.executeQuery(query)
+        self.assertEquals(len(hits), 1)
+        self.assertEquals(['0123456789'], list(hits))
+
     def testAddToIndexWithDuplicateField(self):
         myDocument = Document('id')
         myDocument.addIndexedField('title', 'een titel')
@@ -71,29 +76,12 @@ class LuceneTest(unittest.TestCase):
         self._luceneIndex.addToIndex(myDocument)
 
         query = PyLucene.QueryParser('title', PyLucene.StandardAnalyzer()).parse('titel')
-        hits = self._luceneIndex.query(query)
+        hits = self._luceneIndex.executeQuery(query)
         self.assertEquals(len(hits), 1)
 
         query = PyLucene.QueryParser('title', PyLucene.StandardAnalyzer()).parse('sub')
-        hits = self._luceneIndex.query(query)
+        hits = self._luceneIndex.executeQuery(query)
         self.assertEquals(len(hits), 1)
-
-
-    def testAddToIndex(self):
-        myDocument = Document('0123456789')
-        myDocument.addIndexedField('title', 'een titel')
-        self._luceneIndex.addToIndex(myDocument)
-
-        query = PyLucene.QueryParser('title', PyLucene.StandardAnalyzer()).parse('titel')
-        hits = self._luceneIndex.query(query)
-        self.assertEquals(len(hits), 1)
-        hit = hits[0]
-        i = 0
-        for x in hit.fields():
-            self.assertEquals(IDFIELD, x.name())
-            i = i + 1
-        self.assertEquals(1, i)
-        self.assertEquals(hit.getField(IDFIELD).stringValue(), '0123456789')
 
     def testAddTwoDocuments(self):
         myDocument = Document('1')
@@ -105,7 +93,7 @@ class LuceneTest(unittest.TestCase):
         self._luceneIndex.addToIndex(myDocument)
 
         query = PyLucene.QueryParser('title', PyLucene.StandardAnalyzer()).parse('titel')
-        hits = self._luceneIndex.query(query)
+        hits = self._luceneIndex.executeQuery(query)
         self.assertEquals(2, len(hits))
 
     def testAddDocumentWithTwoValuesForOneField(self):
@@ -116,7 +104,7 @@ class LuceneTest(unittest.TestCase):
 
         def check(value):
             query = PyLucene.QueryParser('field1', PyLucene.StandardAnalyzer()).parse(value)
-            hits = self._luceneIndex.query(query)
+            hits = self._luceneIndex.executeQuery(query)
             self.assertEquals(1, len(hits))
         check('value_1')
         check('value_2')
@@ -135,12 +123,12 @@ class LuceneTest(unittest.TestCase):
         myDocument.addIndexedField('title', 'een titel')
         self._luceneIndex.addToIndex(myDocument)
         query = PyLucene.QueryParser('title', PyLucene.StandardAnalyzer()).parse('titel')
-        hits = self._luceneIndex.query(query)
+        hits = self._luceneIndex.executeQuery(query)
         self.assertEquals(2, len(hits))
 
         self._luceneIndex.deleteID('1')
         query = PyLucene.QueryParser('title', PyLucene.StandardAnalyzer()).parse('titel')
-        hits = self._luceneIndex.query(query)
+        hits = self._luceneIndex.executeQuery(query)
         self.assertEquals(1, len(hits))
 
     def testIndexCloses(self):
@@ -157,74 +145,3 @@ class LuceneTest(unittest.TestCase):
         lockfile = join(self.directoryName, 'write.lock')
         self._luceneIndex.optimize()
         self.assertFalse(isfile(lockfile))
-
-
-    def testCountField(self):
-        self.assertEquals([], self._luceneIndex.countField('title'))
-
-        myDocument = Document('0')
-        myDocument.addIndexedField('title', 'titel')
-        myDocument.addIndexedField('creator', 'one')
-        self._luceneIndex.addToIndex(myDocument)
-
-        my2Document = Document('1')
-        my2Document.addIndexedField('title', 'een titel')
-        my2Document.addIndexedField('creator', 'two')
-        self._luceneIndex.addToIndex(my2Document)
-
-        self.assertEquals([(u'een', 1), (u'titel', 2)], self._luceneIndex.countField('title'))
-
-        self.assertEquals([(u'two', 1), (u'one', 1)], self._luceneIndex.countField('creator'))
-
-
-    def xxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxxxxxtestDeletedDocumentsInFieldCountGlitch(self):
-        """This test is to make sure that values in deleted documents do not appear in the fieldcount output. This problem was noted on 26/04/2007 while working on tagging for LOREnet. The PyLucene version 2.0.0-3 has this problem, version 2.0.0 does not.
-
-        Update 2/8/07: PyLucene 2.2 still has this problem (will it be a feature?).  Since it does only harm luceneIndex.fieldCount(), which is deprecated, we decided not to solve the problem.  In case you want to solve it, read the quite long description that comes with the javadoc for IndexWriter."""
-        directory = mkdtemp()
-        try:
-            document = PyDocument()
-            document.add(Field("id", "1", Field.Store.YES, Field.Index.TOKENIZED))
-            document.add(Field("label", "value", Field.Store.NO, Field.Index.UN_TOKENIZED))
-
-            analyzer = StandardAnalyzer()
-            writer = IndexWriter(directory, analyzer, True)
-            writer.addDocument(document)
-            writer.close()
-
-            reader = IndexReader.open(directory)
-            self.assertFalse(reader.hasDeletions())
-            self.assertEquals(1, reader.docFreq(Term('label', 'value')))
-
-            reader.deleteDocuments(Term('id', '1'))
-
-            self.assertTrue(reader.hasDeletions())
-            self.assertEquals(1, reader.docFreq(Term('label', 'value')))
-            reader.close()
-
-            document = PyDocument()
-            document.add(Field("id", "1", Field.Store.YES, Field.Index.TOKENIZED))
-            document.add(Field("label", "newvalue", Field.Store.NO, Field.Index.UN_TOKENIZED))
-
-            analyzer = StandardAnalyzer()
-            writer = IndexWriter(directory, analyzer, False)
-            writer.addDocument(document)
-            writer.close()
-
-            # The docFreq for Term('label', 'value') should be 0 since the record
-            # got deleted. If this test fails, check the PyLucene version!
-            reader = IndexReader.open(directory)
-            self.assertEquals(0, reader.docFreq(Term('label', 'value')))
-            self.assertEquals(1, reader.docFreq(Term('label', 'newvalue')))
-            reader.close()
-
-            writer = IndexWriter(directory, analyzer, False)
-            writer.optimize()
-            writer.close()
-
-            reader = IndexReader.open(directory)
-            self.assertEquals(0, reader.docFreq(Term('label', 'value')))
-            self.assertEquals(1, reader.docFreq(Term('label', 'newvalue')))
-            reader.close()
-        finally:
-            rmtree(directory)
