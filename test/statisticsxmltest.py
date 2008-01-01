e4584bd80f6b9ff940d1c021501ebc818691c8c3
thijsjanssen <thijsjanssen@83e7a582-4515-4195-8f39-222cbda6297e>
2008-01-01 09:10:51 +0000

    KvS: minor refactoring
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@488 83e7a582-4515-4195-8f39-222cbda6297e

--- test/statisticsxmltest.py
+++ test/statisticsxmltest.py
@@ -9,16 +9,18 @@ class StatisticsXmlTest(CQ2TestCase):
         self.assertEquals(1167609600 + 24 * 60 * 60 - 1, StatisticsXml('ignored')._parseDay('2007-01-01', endDay=True))
 
     def testParseArguments(self):
+        shuntedQuery = []
+        def shuntQuery(beginDay, endDay, key):
+            while shuntedQuery: shuntedQuery.pop()
+            shuntedQuery.append((beginDay, endDay, key))
+            
         def check(expected, query):
             statisticsxml = StatisticsXml('ignored')
-            statisticsxml._query = self.shuntQuery
+            statisticsxml._query = shuntQuery
             statisticsxml.handleRequest(RequestURI='http://localhost/statistics?' + query)
-            self.assertEquals(expected, self.query)
+            self.assertEquals([expected], shuntedQuery)
         check((0, 86399, ('aKey',)), 'key=aKey&beginDay=1970-01-01&endDay=1970-01-01')
         check((0, 86399, ('aKey', 'key2')), 'key=aKey&key=key2&beginDay=1970-01-01&endDay=1970-01-01')
-
-    def shuntQuery(self, beginDay, endDay, key):
-        self.query = beginDay, endDay, key
 
     def testEmptyDataIntegration(self):
         stats = StatisticsXml(Statistics(self.tempdir, [('a',), ('a','b','c')]))
@@ -32,3 +34,5 @@ class StatisticsXmlTest(CQ2TestCase):
 
         result = stats.handleRequest(RequestURI="http://localhost/statistics?key=a&key=b&key=d")
         self.assertEquals(stats._htmlHeader() + """<error>Unknown key: ('a', 'b', 'd')</error>""", ''.join(result))
+
+
