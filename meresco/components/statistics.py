ba219ab700a1360bdbef06f71d205b69957903bb
thijsjanssen <thijsjanssen@83e7a582-4515-4195-8f39-222cbda6297e>
2008-01-02 08:11:55 +0000

    KvS: statistics aggregation - end of day
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@496 83e7a582-4515-4195-8f39-222cbda6297e

--- meresco/components/statistics.py
+++ meresco/components/statistics.py
@@ -159,34 +159,47 @@ class AggregatorException(Exception):
     pass
 
 class AggregatorNode(object):
-    def __init__(self):
+    def __init__(self, lastLasts):
+        self._lastLasts = lastLasts
         self._values = []
         self._children = {}
         self._last = None
         self._aggregated = False
+        self._cleared = False
 
-    def _aggregate(self):
+    def _clear(self):
+        self._values = []
+        self._cleared = True
+
+    def _aggregate(self, depth):
         if self._aggregated:
             return
+        if self._lastLasts[depth]:
+            self._lastLasts[depth]._clear()
+            self._lastLasts[depth] = None
+
         for nr, child in self._children.items():
-            child._aggregate()
+            child._aggregate(depth + 1)
             self._values.extend(child._values)
         self._aggregated = True
+        self._lastLasts[depth] = self
 
-    def add(self, l, data):
+    def add(self, l, data, depth):
         if len(l) == 0:
             self._values.append(data)
         else:
             head, tail = l[0], l[1:]
             if not head in self._children:
-                self._children[head] = AggregatorNode()
-            self._children[head].add(tail, data)
+                self._children[head] = AggregatorNode(self._lastLasts)
+            self._children[head].add(tail, data, depth + 1)
             if self._last != None and self._last != head:
-                self._children[self._last]._aggregate()
+                self._children[self._last]._aggregate(depth + 1)
             self._last = head
 
     def get(self, l):
         if len(l) == 0:
+            if self._cleared:
+                raise AggregatorException('too precise')
             for value in self._values:
                 yield value
             if not self._aggregated:
@@ -205,13 +218,13 @@ class AggregatorNode(object):
 class Aggregator(object):
 
     def __init__(self):
-        self._root = AggregatorNode()
+        self._root = AggregatorNode([None] * 7)
 
     def add(self, data):
         self._addAt(gmtime()[:6], data)
 
     def _addAt(self, time, data):
-        self._root.add(time, data)
+        self._root.add(time, data, 0)
 
     def get(self, fromTime):
         return compose(self._root.get(fromTime))
