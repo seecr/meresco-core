84b86f828157b30df0d314a280ad0a65d1303af1
erik <erik@83e7a582-4515-4195-8f39-222cbda6297e>
2008-01-03 19:34:33 +0000

    EG: fixed mem/perf experiment test
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@519 83e7a582-4515-4195-8f39-222cbda6297e

--- test/statswithtuples.py
+++ test/statswithtuples.py
@@ -1,10 +1,10 @@
 ## begin license ##
 #
-#    Meresco Core is an open-source library containing components to build 
+#    Meresco Core is an open-source library containing components to build
 #    searchengines, repositories and archives.
 #    Copyright (C) 2007-2008 Seek You Too (CQ2) http://www.cq2.nl
 #    Copyright (C) 2007-2008 SURF Foundation. http://www.surf.nl
-#    Copyright (C) 2007-2008 Stichting Kennisnet Ict op school. 
+#    Copyright (C) 2007-2008 Stichting Kennisnet Ict op school.
 #       http://www.kennisnetictopschool.nl
 #    Copyright (C) 2007 SURFnet. http://www.surfnet.nl
 #
@@ -26,24 +26,33 @@
 #
 ## end license ##
 from unittest import TestCase, main
-from itertools import groupby, takewhile, dropwhile
+from itertools import groupby, takewhile, dropwhile, chain
 
 def _commonPrefix(t0, t1):
     return tuple(takewhile(lambda (l,r): l>=r, zip(t0, t1)))
 
-def _collapse(time, t1):
-    return time[:len(_commonPrefix(time, t1)) + 1]
+def _collapse(time, t0):
+    return time[:len(_commonPrefix(time, t0)) + 1]
 
-def _groupbytime(observations, time):
-    return groupby(observations, lambda (t, observation): _collapse(t, time))
+def _groupbytime(data, t0):
+    return groupby(data, lambda (time, observations): _collapse(time, t0))
 
-def _merge(observations):
-    O = sorted(observations, lambda (keylhs,valuelhs), (keyrhs,valuerhs): cmp(valuelhs, valuerhs))
-    return tuple((value, sum(count for value, count in group)) for value, group in groupby((observation for time, observation in O), lambda (value, count): value))
+def _sortbyobservation(observations):
+    return sorted(observations, lambda (valuelhs,occurrencelhs), (valuerhs,occurrencerhs): cmp(valuelhs, valuerhs))
+
+def _groupbyobservation(data):
+    observations = chain(*(observations for time, observations in data))
+    return groupby(_sortbyobservation(observations), lambda (value, occurrence): value)
+
+def _countobservations(observations):
+    return sum(occurrence for value, occurrence in observations)
+
+def _merge(data):
+    return tuple((value, _countobservations(observations)) for value, observations in _groupbyobservation(data))
 
 def consolidate(data, collapseBefore=(0,0,0,0,0,0)):
-    for statistic, occurrences in _groupbytime(data, collapseBefore):
-        yield statistic, _merge(occurrences)
+    for time, data in _groupbytime(data, collapseBefore):
+        yield time, _merge(data)
 
 def select(data, start, until):
     return takewhile(lambda (time, observations): time < until,
@@ -56,16 +65,32 @@ class TestConsolidate(TestCase):
         for item in lhs:
             self.assertEquals(item, rhs.next())
 
+    def testClarifyDataStructure(self):
+        t0 = (1,2,3)
+        value0 = ('a','b')
+        occurrence0 = 3
+        observation0 = (value0, occurrence0)
+        value1 = ('c','d')
+        occurrence1 = 2
+        observation1 = (value1, occurrence1)
+        observations0 = (observation0, observation1)
+        exampledata = [(t0, observations0),]
+        self.assertEquals([(t0,(observation0, observation1)) ], exampledata)
+        self.assertEquals([(t0,(observation0, (value1, occurrence1))) ], exampledata)
+        self.assertEquals([(t0,(observation0, (value1, 2))) ], exampledata)
+        self.assertEquals([(t0,((('a','b'),3), (('c','d'), 2))) ], exampledata)
+        self.assertEquals([((1,2,3),((('a','b'),3),(('c','d'),2)))], exampledata)
+
     def testGetStatistics(self):
         data = [
-            ((1,2,3),(('c','c'),1)),
-            ((2,3,4,1),(('a','b'),1)),
-            ((3,4,5,2,3),(('c','c'),1)),
-            ((4,5,6,4,5,6),(('a','b'),1)),
+            ((1,2,3),((('c','c'),1),)),
+            ((2,3,4,1),((('a','b'),1),)),
+            ((3,4,5,2,3),((('c','c'),1),)),
+            ((4,5,6,4,5,6),((('a','b'),1),)),
         ]
         p = select(data, start=(2,3,4), until=(3,4,6))
         s = consolidate(p, (999,))
-        self.assertEquals('?', list(s))
+        self.assertEquals([((2,), ((('a','b'),1),)), ((3,), ((('c','c'),1),),)], list(s))
 
     def testSelectPeriod(self):
         data = [
@@ -85,22 +110,22 @@ class TestConsolidate(TestCase):
 
     def testValuesAreSorted(self):
         c = consolidate([
-            ((1,2,1),(('c','c'),1)),
-            ((1,2,2),(('a','b'),1)),
-            ((1,4,3),(('c','c'),1)),
-            ((1,4,4),(('a','b'),1)),
+            ((1,2,1),((('c','c'),1),(('a','b'),1))),
+            ((1,2,2),((('a','b'),1),)),
+            ((1,4,3),((('c','c'),1),)),
+            ((1,4,4),((('a','b'),1),)),
         ], collapseBefore=(2,))
         self.assertEqualsData([
-            ((1,), ((('a','b'),2), (('c','c'),2))),
+            ((1,), ((('a','b'),3), (('c','c'),2))),
         ], c)
 
     def  testWhat(self):
         c = consolidate([
-            ((1,2,1),(('a','b'),1)),
-            ((2,2,2),(('a','b'),1)),
-            ((2,2,3),(('a','c'),1)),
-            ((2,3,1),(('a','c'),1)),
-            ((3,4,4),(('a','d'),1)),
+            ((1,2,1),((('a','b'),1),)),
+            ((2,2,2),((('a','b'),1),)),
+            ((2,2,3),((('a','c'),1),)),
+            ((2,3,1),((('a','c'),1),)),
+            ((3,4,4),((('a','d'),1),)),
         ], collapseBefore=(2,3,0))
         self.assertEqualsData([
             ((1,), ((('a','b'),1),)),
@@ -111,11 +136,11 @@ class TestConsolidate(TestCase):
 
     def testConsolidateConditionallyPartially(self):
         c = consolidate([
-            ((1,2,1,1),(('a','b'),1)),
-            ((1,2,1,2),(('a','b'),1)),
-            ((1,2,2,1),(('a','b'),1)),
-            ((1,2,3),(('a','c'),1)),
-            ((1,4,4),(('a','d'),1)),
+            ((1,2,1,1),((('a','b'),1),)),
+            ((1,2,1,2),((('a','b'),1),)),
+            ((1,2,2,1),((('a','b'),1),)),
+            ((1,2,3),((('a','c'),1),)),
+            ((1,4,4),((('a','d'),1),)),
         ], collapseBefore=(1,2,3))
         self.assertEqualsData([
             ((1,2,1), ((('a','b'),2),)),
@@ -126,10 +151,10 @@ class TestConsolidate(TestCase):
 
     def testConsolidateConditionallyTwo(self):
         c = consolidate([
-            ((1,2,1),(('a','b'),1)),
-            ((1,2,2),(('a','b'),2)),
-            ((1,4,3),(('a','c'),1)),
-            ((1,4,4),(('a','d'),2)),
+            ((1,2,1),((('a','b'),1),)),
+            ((1,2,2),((('a','b'),2),)),
+            ((1,4,3),((('a','c'),1),)),
+            ((1,4,4),((('a','d'),2),)),
         ], collapseBefore=(1,3))
         self.assertEqualsData([
             ((1,2), ((('a','b'),3),)),
@@ -139,9 +164,9 @@ class TestConsolidate(TestCase):
 
     def testConsolidateConditionallyOne(self):
         c = consolidate([
-            ((1,2,1),(('a','b'),3)),
-            ((1,3,2),(('a','b'),2)),
-            ((1,4,3),(('a','c'),1)),
+            ((1,2,1),((('a','b'),3),)),
+            ((1,3,2),((('a','b'),2),)),
+            ((1,4,3),((('a','c'),1),)),
         ], collapseBefore=(1,3,2))
         a = c.next()
         self.assertEquals(((1,2), ((('a','b'),3),)), a)
@@ -152,9 +177,9 @@ class TestConsolidate(TestCase):
 
     def testConsolidateLowerLevel(self):
         c = consolidate([
-            ((1,2,1),(('a','b'),1)),
-            ((1,2,2),(('a','b'),1)),
-            ((1,2,3),(('a','c'),1)),
+            ((1,2,1),((('a','b'),1),)),
+            ((1,2,2),((('a','b'),1),)),
+            ((1,2,3),((('a','c'),1),)),
         ])
         a = c.next()
         self.assertEquals(((1,2,1), ((('a','b'),1),)), a)
@@ -165,9 +190,9 @@ class TestConsolidate(TestCase):
 
     def testConsolidateHigherLevel(self):
         c = consolidate([
-            ((1,2),(('a','b'),3)),
-            ((1,3),(('a','b'),2)),
-            ((2,3),(('a','b'),1)),
+            ((1,2),((('a','b'),3),)),
+            ((1,3),((('a','b'),2),)),
+            ((2,3),((('a','b'),1),)),
         ], (9,9))
         a = c.next()
         self.assertEquals(((1,), ((('a','b'),5),)), a)
@@ -176,11 +201,11 @@ class TestConsolidate(TestCase):
 
     def testConsolidateMultipleOccurencesPerPeriod(self):
         c = consolidate([
-            ((1,2),(('a','b'),1)),
-            ((1,2,2,3,4,5,6),(('a','b'),1)),
-            ((1,2),(('a','c'),1)),
-            ((1,3),(('a','b'),1)),
-            ((1,3),(('a','b'),1))
+            ((1,2),((('a','b'),1),)),
+            ((1,2,2,3,4,5,6),((('a','b'),1),)),
+            ((1,2),((('a','c'),1),)),
+            ((1,3),((('a','b'),1),)),
+            ((1,3),((('a','b'),1),))
         ], (1,4))
         a = c.next()
         self.assertEquals(((1,2), ((('a','b'),2), (('a','c'),1))), a)
@@ -189,11 +214,11 @@ class TestConsolidate(TestCase):
 
     def testConsolidateMixed(self):
         c = consolidate([
-            ((1,2),(('a','b'),1)),
-            ((1,2),(('a','b'),2)),
-            ((1,2),(('c','d'),3)),
-            ((1,3,4,5,6),(('a','b'),2)),
-            ((1,3),(('c','d'), 1))
+            ((1,2),((('a','b'),1),)),
+            ((1,2),((('a','b'),2),)),
+            ((1,2),((('c','d'),3),)),
+            ((1,3,4,5,6),((('a','b'),2),)),
+            ((1,3),((('c','d'), 1),))
         ], (1,5))
         a = c.next()
         self.assertEquals(((1,2), ((('a','b'),3), (('c','d'),3))), a)
@@ -202,9 +227,9 @@ class TestConsolidate(TestCase):
 
     def testConsolidateDifferentTimePeriods(self):
         c = consolidate([
-            ((1,2),(('a','b'),1)),
-            ((1,3),(('c','d'),2)),
-            ((1,4),(('e','f'),1))
+            ((1,2),((('a','b'),1),)),
+            ((1,3),((('c','d'),2),)),
+            ((1,4),((('e','f'),1),))
         ])
         a = c.next()
         self.assertEquals(((1,2), ((('a','b'),1),)), a)
@@ -215,18 +240,18 @@ class TestConsolidate(TestCase):
 
     def testConsolidateOneTimePeriod(self):
         c = consolidate([
-            ((1,2),(('a','b'),1)),
-            ((1,2),(('c','d'),1)),
-            ((1,2),(('e','f'),1))
+            ((1,2),((('a','b'),1),)),
+            ((1,2),((('c','d'),1),)),
+            ((1,2),((('e','f'),1),))
         ])
         a = c.next()
         self.assertEquals(((1,2), ((('a','b'),1), (('c','d'),1), (('e','f'),1))), a)
 
-    def XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXtestToExperimentWith(self):
+    def testToExperimentWith(self):
         from random import randint
         from time import gmtime, time
         from sys import maxint
-        c = consolidate([(gmtime()[:6], (str(randint(0,1000)),)) for ip in xrange(100000)], (2008,1,3,8,41))
+        c = consolidate([(gmtime()[:6], (((str(randint(0,10))),1),)) for ip in xrange(10000)], (2008,1,3,8,41))
         #raw_input('before')
         print '\n'
         t0 = time()
