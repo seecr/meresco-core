1ad2e2306703fc0e2b383f05769648c239d501e6
thijsjanssen <thijsjanssen@83e7a582-4515-4195-8f39-222cbda6297e>
2008-01-11 12:49:10 +0000

    KvS: Drilldown bug resolved (Drilldown exception typo)
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@540 83e7a582-4515-4195-8f39-222cbda6297e

--- meresco/components/drilldown/drilldown.py
+++ meresco/components/drilldown/drilldown.py
@@ -1,10 +1,10 @@
 ## begin license ##
 #
-#    Meresco Core is an open-source library containing components to build 
+#    Meresco Core is an open-source library containing components to build
 #    searchengines, repositories and archives.
 #    Copyright (C) 2007-2008 Seek You Too (CQ2) http://www.cq2.nl
 #    Copyright (C) 2007-2008 SURF Foundation. http://www.surf.nl
-#    Copyright (C) 2007-2008 Stichting Kennisnet Ict op school. 
+#    Copyright (C) 2007-2008 Stichting Kennisnet Ict op school.
 #       http://www.kennisnetictopschool.nl
 #    Copyright (C) 2007 SURFnet. http://www.surfnet.nl
 #
@@ -27,7 +27,7 @@
 ## end license ##
 from bitmatrix import BitMatrix
 
-class DrillDownException(Exception):
+class DrilldownException(Exception):
     pass
 
 class FieldMatrix(object):
