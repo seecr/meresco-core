cbbacde918c0fa29d5c41478d31ba598c7601604
thijsjanssen <thijsjanssen@83e7a582-4515-4195-8f39-222cbda6297e>
2008-02-19 16:31:52 +0000

    JJ: testcase to show slow performance drilldown based on tillburg index
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@610 83e7a582-4515-4195-8f39-222cbda6297e

---
+++ test/drilldownperformancetest.py
@@ -0,0 +1,46 @@
+import os, sys
+from os.path import isfile, join
+from glob import glob
+from time import time
+
+os.system('find .. -name "*.pyc" | xargs rm -f')
+
+for path in glob('../deps.d/*'):
+    sys.path.insert(0, path)
+sys.path.insert(0, '..')
+
+INDEX_PATH='/home/pair/zandbak/index'
+
+lockFile = join(INDEX_PATH, 'write.lock')
+if isfile(lockFile):
+    os.remove(lockFile)
+
+from meresco.components.lucene import LuceneIndex
+from meresco.components.drilldown import Drilldown
+
+from PyLucene import TermQuery, Term
+
+drilldownFields = [
+    'drilldown.olc.yearNorm',
+#    'drilldown.olc.author',
+#    'drilldown.olc.source',
+#    'drilldown.olc.uncontrolledTerm'
+]
+
+t0 = time()
+index = LuceneIndex(INDEX_PATH, 'cqlparser', 'reactor')
+drilldown = Drilldown(drilldownFields)
+drilldown.indexOptimized(index._reader)
+print "Open index and drilldown in", time() - t0
+
+
+query = TermQuery(Term('meta.repository.collection', 'tilburg'))
+
+t0 = time()
+hits = index.executeQuery(query)
+docIds = hits.docNumbers()
+for fieldname, results in drilldown.drilldown(docIds, zip(drilldownFields, [10] * len(drilldownFields))):
+    print fieldname
+    print list(results)
+
+print 'drilldown time', time() - t0
