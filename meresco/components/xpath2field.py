82cf8c94b0a1c7c1b2e24dc8887e2af0ec2ce848
johan_jonkers <johan_jonkers@83e7a582-4515-4195-8f39-222cbda6297e>
2008-07-28 15:02:56 +0000

    EG/JJ: xpath2field works, going to be used in delft
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@1008 83e7a582-4515-4195-8f39-222cbda6297e

--- meresco/components/xpath2field.py
+++ meresco/components/xpath2field.py
@@ -1,5 +1,5 @@
 from meresco.framework import Observable
-from meresco.components.dictionary import DocumentDict
+
 
 class XPath2Field(Observable):
     def __init__(self, attributeXpaths=[], namespaceMap={}):
@@ -8,9 +8,8 @@ class XPath2Field(Observable):
         self._namespaceMap = namespaceMap
 
     def add(self, id, partName, lxmlNode):
-        dd = DocumentDict()
         for (xpath, dottedDestinationPath) in self._attributeXpaths:
-            attribute = lxmlNode.xpath(xpath, namespaces=self._namespaceMap)
-            if attribute:
-                dd.add(dottedDestinationPath, attribute[0])
-        return self.all.addDocumentDict(id, dd)
+            values = lxmlNode.xpath(xpath, namespaces=self._namespaceMap)
+            if values:
+                self.do.addField(name=dottedDestinationPath, value=values)
+        return self.all.add(self, id, partName, lxmlNode)
--- test/xpath2fieldtest.py
+++ test/xpath2fieldtest.py
@@ -6,7 +6,7 @@ from lxml.etree import parse
 from StringIO import StringIO
 
 class XPath2FieldTest(CQ2TestCase):
-   def testXPath2Field(self):
+   def testSingleXPath2Field(self):
         observer = CallTrace()
         xml2dict = XPath2Field([('/a/b//@attr', 'a.b.attr')], namespaceMap={'ns': 'http://namespace.org/'})
         xml2dict.addObserver(observer)
@@ -18,5 +18,33 @@ class XPath2FieldTest(CQ2TestCase):
         </a>"""))
 
         list(xml2dict.add('id', 'partname', node))
-        documentDict = observer.calledMethods[0].args[1]
-        self.assertEquals('The Attribute', documentDict.get('a.b.attr')[0].value)
+        self.assertEquals(2, len(observer.calledMethods))
+        addFieldMethod = observer.calledMethods[0]
+        self.assertEquals('add', observer.calledMethods[1].name)
+        self.assertEquals('addField', addFieldMethod.name)
+        self.assertEquals('a.b.attr', addFieldMethod.kwargs['name'])
+        self.assertEquals(['The Attribute'], addFieldMethod.kwargs['value'])
+        
+   def testMultipleXPath2Field(self):
+        observer = CallTrace()
+        xml2dict = XPath2Field([('/a/b//@attr', 'a.b.attr')], namespaceMap={'ns': 'http://namespace.org/'})
+        xml2dict.addObserver(observer)
+
+        node = parse(StringIO("""<a>
+            <b attr="attr_1">
+               content of the b tag
+            </b>
+            <b attr="attr_2">
+               content of the b tag
+            </b>
+            <b attr="attr_3">
+               content of the b tag
+            </b>
+        </a>"""))
+
+        list(xml2dict.add('id', 'partname', node))
+        self.assertEquals(2, len(observer.calledMethods))
+        addFieldMethod = observer.calledMethods[0]
+        self.assertEquals('addField', addFieldMethod.name)
+        self.assertEquals('a.b.attr', addFieldMethod.kwargs['name'])
+        self.assertEquals(['attr_1', 'attr_2', 'attr_3'], addFieldMethod.kwargs['value'])
