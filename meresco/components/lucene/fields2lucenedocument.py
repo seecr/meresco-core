236093dd34882d2ef50f609a6cd7cd8ae80d4505
thijsj <thijsj@83e7a582-4515-4195-8f39-222cbda6297e>
2008-08-01 14:09:34 +0000

    TJ/JJ: key in name.
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@1022 83e7a582-4515-4195-8f39-222cbda6297e

--- meresco/components/lucene/fields2lucenedocument.py
+++ meresco/components/lucene/fields2lucenedocument.py
@@ -38,8 +38,8 @@ class Fields2LuceneDocument(Observable):
     def begin(self):
         self.txs[self.tx.getId()] = Fields2LuceneDocumentTx(self, self._untokenized)
 
-    def addField(self, key, value):
-        self.txs[self.tx.getId()].addField(key, value)
+    def addField(self, name, value):
+        self.txs[self.tx.getId()].addField(name, value)
 
     def commit(self):
         self.txs[self.tx.getId()].finalize()
@@ -53,17 +53,17 @@ class Fields2LuceneDocumentTx(object):
         self.fields = {}
         self._untokenized = untokenized
 
-    def addField(self, key, value):
-        if not key in self.fields:
-            self.fields[key] = []
-        self.fields[key].append(value)
+    def addField(self, name, value):
+        if not name in self.fields:
+            self.fields[name] = []
+        self.fields[name].append(value)
 
     def finalize(self):
         document = Document(self.fields['__id__'][0])
         del self.fields['__id__']
-        for key, values in self.fields.items():
+        for name, values in self.fields.items():
             for value in values:
-                document.addIndexedField(key, value, not key in self._untokenized)
+                document.addIndexedField(name, value, not name in self._untokenized)
         self.parent.do.addDocument(document)
 
 
