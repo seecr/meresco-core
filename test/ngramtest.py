7d67267dcc8e608c5cf8de7b6b683e1209f3e6a0
johan_jonkers <johan_jonkers@83e7a582-4515-4195-8f39-222cbda6297e>
2008-10-20 15:05:54 +0000

    EG/JJ: working on ngrams
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco/trunk@1091 83e7a582-4515-4195-8f39-222cbda6297e

--- test/ngramtest.py
+++ test/ngramtest.py
@@ -1,4 +1,4 @@
-from cq2utils import CQ2TestCase
+from cq2utils import CQ2TestCase, CallTrace
 from time import time
 from sys import stdout
 from PyLucene import IndexReader, IndexWriter, IndexSearcher, BooleanQuery, BooleanClause, TermQuery, Term, Field, StandardAnalyzer, Document
@@ -21,7 +21,40 @@ def ngramQuery(word, N=2):
         query.add(BooleanClause(TermQuery(Term('ngrams', ngram)), BooleanClause.Occur.SHOULD))
     return query
 
+
+
+
+from meresco.framework import Transparant
+
+class NGramFieldlet(Transparant):
+    def __init__(self, ngrams, termField, ngramField):
+        Transparant.__init__(self)
+        self._ngrams = ngrams
+        self._termField = termField
+        self._ngramField = ngramField
+
+    def addField(self, field, value):
+        for word in value.split():
+            self.do.addField(self._termField, word)
+            for ngram in self._ngram(word):
+                self.do.addField(self._ngramField, ngram)
+
+    def _ngram(self, word):
+        for n in range(2, self._ngrams+1):
+            for i in range(len(word)-n+1):
+                yield word[i:i+n]
+
+
 class NGramTest(CQ2TestCase):
+    def testDNA(self):
+        dna = \
+            (NGramFieldlet(2, 'word', 'ngrams'),
+                (TransactionFactory(lambda tx: Fields2LuceneDocumentTx(tx, untokenized=['word', 'ngrams'])),
+                    (LuceneIndex(self.tempdir, mockreactor),)
+                )
+            )
+        x = be(dna)
+        self.assertFalse('hier verder')
 
     def testCreateIndex(self):
         index = IndexWriter(self.tempdir, StandardAnalyzer(), True)
@@ -35,13 +68,28 @@ class NGramTest(CQ2TestCase):
         self.assertEquals(set(['bo', 'oo', 'om', 'boo', 'oom', 'boom']), set(ngrams('boom', N=4)))
         self.assertEquals(set(['bo', 'oo', 'om']), set(ngrams('boom', N=2)))
 
-    def testWords(self):
+    def testNGramFieldLet(self):
+        observert = CallTrace('Observert')
+        ngramFieldlet = NGramFieldlet(2, 'word', 'ngrams')
+        ngramFieldlet.addObserver(observert)
+
+        ngramFieldlet.addField('field0', 'term0')
+        self.assertEquals(5, len(observert.calledMethods))
+        self.assertEquals("addField('word', 'term0')", str(observert.calledMethods[0]))
+        self.assertEquals("addField('ngrams', 'te')", str(observert.calledMethods[1]))
+        self.assertEquals("addField('ngrams', 'er')", str(observert.calledMethods[2]))
+        self.assertEquals("addField('ngrams', 'rm')", str(observert.calledMethods[3]))
+        self.assertEquals("addField('ngrams', 'm0')", str(observert.calledMethods[4]))
+
+
+    def testIntegrationWords(self):
         #index = IndexWriter('index', StandardAnalyzer(), True)
-        #f = open('words')
+        #f = open('/usr/share/dict/words')
         #for word in f:
             ##print word
             #addWord(index, word.strip().decode('iso 8859-1'))
         #index.flush()
+        #index.close()
         searcher = IndexSearcher('index')
         # Alabama
         for word in ['Alabama', 'alhambra', 'albuqurqi', 'matematics', 'restauration', 'abridgement', 'entousiast', 'puch', 'grnt', 'carot', 'from', 'sema', 'bord', 'enrgie', 'energie', 'enery', 'energy' ]:
@@ -56,7 +104,7 @@ class NGramTest(CQ2TestCase):
                     suggestions.append((term, score, distance(unicode(term), unicode(word)), ratio(unicode(term), unicode(word))))
                 levenSuggs = sorted(suggestions, key=lambda x: x[2])[:5]
                 ratioSuggs = sorted(suggestions, key=lambda x: x[3], reverse=True)[:5]
-                #print '    Score:', ', '.join('%s (%.1f)' % (sugg[0], sugg[1]) for sugg in suggestions[:5])
+                print '    Score:', ', '.join('%s (%.1f)' % (sugg[0], sugg[1]) for sugg in suggestions[:5])
                 print '    Leven (n=%d):'%N, ', '.join('%s (%.1f)' % (sugg[0], sugg[2]) for sugg in levenSuggs if sugg[2] < 5)
                 print '    Ratio (n=%d):'%N, ', '.join('%s (%.1f)' % (sugg[0], sugg[3]) for sugg in ratioSuggs if sugg[3] > 0.65)
 
