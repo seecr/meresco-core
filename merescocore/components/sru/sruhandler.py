e006e10bb59a5e5148c6943758df83a1a1a92336
thijsj <thijsj@83e7a582-4515-4195-8f39-222cbda6297e>
2010-02-25 13:56:55 +0000

    TS/TJ: sru handler will now give a better error when recordSchema does not exist.
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco-core/workingsets/2.22.9-EduRep/version_2-storage-optimization@2048 83e7a582-4515-4195-8f39-222cbda6297e

--- merescocore/components/sru/sruhandler.py
+++ merescocore/components/sru/sruhandler.py
@@ -135,7 +135,7 @@ class SruHandler(Observable):
         yield '<srw:extraRecordData>'
         for schema in x_recordSchema:
             yield '<recordData recordSchema="%s">' % xmlEscape(schema)
-            yield self._catchErrors(self._yieldRecordForRecordPacking(recordId, schema, recordPacking), x_recordSchema, recordId)
+            yield self._catchErrors(self._yieldRecordForRecordPacking(recordId, schema, recordPacking), schema, recordId)
             yield '</recordData>'
         yield '</srw:extraRecordData>'
 
